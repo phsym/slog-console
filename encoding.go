3a2a41687bec822dd140e61fd0ab7be13045ee72
Russ Egan <russell.egan@thalesgroup.com>
2025-01-07 15:24:01 -0600

    Use fmt for errors, like slog.TextHandler
    Uses fmt.Fprintf with "%+v" to print errors.  Useful with some error packages which render additional information like stacktraces.  Addresses #7

--- encoding.go
+++ encoding.go
@@ -144,7 +144,9 @@ func (e encoder) writeValue(buf *buffer, value slog.Value) {
 	case slog.KindAny:
 		switch v := value.Any().(type) {
 		case error:
-			e.writeColoredString(buf, v.Error(), e.opts.Theme.AttrValueError())
+			e.withColor(buf, e.opts.Theme.AttrValueError(), func() {
+				fmt.Fprintf(buf, "%+v", v)
+			})
 			return
 		case fmt.Stringer:
 			e.writeColoredString(buf, v.String(), attrValue)
